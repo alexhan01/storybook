758543e465ae3268515b076c184c917243b56966
a-x-hn <alexhan0324@gmail.com>
2020-09-02 04:26:30 -0700

    added fontawesome

--- src/components/nav/sidebar.js
+++ src/components/nav/sidebar.js
@@ -2,13 +2,15 @@ import React, { Component } from "react"
 
 import Navigation from "./navigation"
 import styles from "./sidebar.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 class Sidebar extends Component {
     render() {
         return (
             <>
                 <button className={styles.burger} onClick={() => this.toggleMenu()}>
-                    Open
+                    <FontAwesomeIcon icon={faBars} />
                 </button>
 
                 <Navigation ref={el => (this.childMenu = el)} />
